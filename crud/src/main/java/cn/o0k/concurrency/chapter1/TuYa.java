e979c71b1bc0ad4b1077a442af6a8dc3b74f8c4c
zhangpeng <zhangpeng03@wondersgroup.com>
2020-09-01 15:01:33 +0800

    新增：售票机 描述：学习多线程

--- crud/src/main/java/cn/o0k/concurrency/chapter1/TuYa.java
+++ crud/src/main/java/cn/o0k/concurrency/chapter1/TuYa.java
@@ -1,22 +1,32 @@
 package cn.o0k.concurrency.chapter1;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author 张澎_9970
  * @DESCRIPTION:
  * @DATE: 2020/8/16 20:15 星期日
  */
 public class TuYa {
-	public static void main(String[] args) {
-		new TuYa().mm();
-		{
-			System.out.println("I am code block.");
-		}
+    public static void main(String[] args) {
+        new TuYa().mm();
+        {
+            System.out.println("sleep two minutes.");
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                System.out.println("22");
+                e.printStackTrace();
+            }
+            System.out.println("It wakes.");
+            System.out.println("I am code block.");
+        }
 
-	}
+    }
 
-	public synchronized void mm() {
+    public synchronized void mm() {
 
-		System.out.println("synchronized");
-	}
-	// Small is power, small is beautiful!
+        System.out.println("synchronized");
+    }
+    // Small is power, small is beautiful!
 }
