c100af4eb96b40512d97391229dee3436681c896
张澎_9970 <zhangpeng03@wondersgroup.com>
2021-04-04 23:56:31 +0800

    新增: 1.两数之和

--- crud/src/main/java/SunFa/Algorithms.java
+++ crud/src/main/java/SunFa/Algorithms.java
@@ -2,6 +2,8 @@ package SunFa;
 
 import org.apache.commons.lang3.builder.ToStringExclude;
 
+import java.util.Arrays;
+
 /**
  * @author 张澎_9970
  * @date 2021-04-03 21:40 星期六
@@ -10,19 +12,56 @@ import org.apache.commons.lang3.builder.ToStringExclude;
 public class Algorithms {
 
     static int[] arr = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    static int[] a = new int[2];
 
     public static void main(String[] args) {
         Algorithms algorithms = new Algorithms();
 
-        algorithms.climbStairs(10);
+        int[] ints = algorithms.twoSum(arr, 10);
+        System.out.println(Arrays.toString(ints));
 
     }
+
+    /**
+     * 1.
+     * 两数之和
+     */
+    public int[] twoSum(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    a[0] = i;
+                    a[1] = j;
+                    return a;
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    /**
+     * 15.
+     * 三数之和
+     */
+
+
+
+
 
     /**
      * 最近重复子问题
      * 1. 1 + 1 + 1
      * 2. 1 + 2
      * 3. 2 + 1
+     *
+     * 核心问题: 你来到这个台阶的方法, 从前面一个台阶, 或者从前面2个台阶.
+     * 解题: 只要最后的三个值:
+     *      result = f1 + f2
+     *      f1 = f2
+     *      f2 = result
+     *
+     *      1.暴力
+     *      2.最简单的情况怎么解决
      *
      * @param n
      * @return
